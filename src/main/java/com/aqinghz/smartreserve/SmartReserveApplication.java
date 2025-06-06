b7a82eac58170d17d761e5a95cf58a9591fb0886
sansuiqiang <1141192431@qq.com>
2025-06-06 20:08:04 +0800

    first upload

---
+++ src/main/java/com/aqinghz/smartreserve/SmartReserveApplication.java
@@ -0,0 +1,13 @@
+package com.aqinghz.smartreserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartReserveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartReserveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aqinghz/smartreserve/SmartReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aqinghz.smartreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartReserveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
